805676fbcfaabd9995b27ec83070a08f8926d016
FeelHippo <mezzasegolas@gmail.com>
2019-12-13 16:57:53 +0100

    first assignment completed

---
+++ first.js
@@ -0,0 +1,57 @@
+class Number{
+    constructor (num) {
+      this.num = num
+    }
+    
+    contains_self(converted) {
+      let repeats = {
+            Foo: 0,
+            Bar: 0,
+            Quix: 0
+          }
+        repeats["Foo"] = converted.filter(x => x == 3).length;
+        repeats["Bar"] = converted.filter(x => x == 5).length;
+        repeats["Quix"] = converted.filter(x => x == 7).length;
+        return repeats;
+    }
+    
+    div_three() {
+      return (this.num%3==0);
+    }
+    
+    div_five() {
+      return (this.num%5==0);
+    }
+    
+    div_seven() {
+      return (this.num%7==0);
+    }
+    
+    div_both() {
+      return ((this.num%5==0) && (this.num%7==0));
+    }
+    
+    string() {
+      let temp_string = (this.div_three()) ? "Foo " : "";
+      temp_string += ((this.div_five()) && (this.div_seven())) ? "Bar Quix " : (this.div_five()) ? "Bar " : (this.div_seven()) ? "Quix " : "";
+     
+      if (this.num.toString().split("").some(x => x == "3"|| x == "5"|| x == "7")) {
+            let times = this.contains_self(this.num.toString().split(""));
+            temp_string = temp_string.split(" ");
+            temp_string = temp_string.map(z => z.concat(z.repeat(times[z]))).join("").trim();
+        
+      }
+      return temp_string;
+    }
+  
+    result() {
+      if (this.string()=="") {return this.num}
+      else {return this.string()}
+    }
+  }
+  
+  
+  const example = new Number(35);
+  
+  
+  console.log(example.result());
