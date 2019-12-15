3ef5795d24f2a1f558f81d04b92ff52cee8b7bc4
FeelHippo <mezzasegolas@gmail.com>
2019-12-15 13:28:33 +0100

    implemented arabic to roman

--- second.js
+++ second.js
@@ -19,7 +19,6 @@ const  priority = [
   function roman_arabic (str) {
       str = str.split("");    
       let arabic = [];
-      console.log(priority[0]["roman"])
       
       while (!str.length==0) {
       if (priority.some(x => x.roman == str.slice(-2).join(""))) {
@@ -48,3 +47,49 @@ const  priority = [
   
   
   console.log(roman_arabic ("MCCXLIV"))
+
+  // Arabic to Roman
+
+  
+function arabic_roman (num) {
+    num = num.toString().split("");
+    let roman = [];
+    
+    while (!num.length==0) {
+      if (priority.some(x => x.arabic == num.slice(-2).join(""))) {
+        roman.push(num.slice(-2).join(""));
+        num.splice(-2, 2);
+      } else {
+        roman.push(num[num.length-1]);
+        num.pop();
+      }
+    }
+    
+    roman[0] = (roman[0]==4) 
+    ? priority[0].roman : (roman[0]==9) 
+    ? priority[1].roman : (roman[0]==5) 
+    ? single_digit[1].roman : (roman[0]<5) 
+    ? single_digit[0].roman.repeat(Number(roman[0])) 
+    : single_digit[1].roman + single_digit[0].roman.repeat(Number(roman[0])%5);
+
+  
+    oman[1] = (roman[1]==4) 
+    ? priority[2].roman : (roman[1]==9) 
+    ? priority[3].roman : (roman[1]==5) 
+    ? single_digit[3].roman : (roman[1]<5) 
+    ? single_digit[2].roman.repeat(Number(roman[1]))
+    : single_digit[3].roman + single_digit[2].roman.repeat(Number(roman[1])%5);
+  
+    roman[2] = (roman[2]==5) 
+    ? single_digit[5].roman : (roman[2]<5) 
+    ? single_digit[4].roman.repeat(parseInt(roman[2])) 
+    : single_digit[5].roman + single_digit[4].roman.repeat(parseInt(roman[2])%5);
+ 
+    roman[3] = single_digit[6].roman.repeat(parseInt(roman[3]));
+  
+  
+    return roman.reverse().join("");
+  
+}
+
+console.log(arabic_roman (1246))
