d48a523929928a90965542285ecb0c92c48234f2
FeelHippo <mezzasegolas@gmail.com>
2019-12-21 15:39:04 +0100

    implemented all minor functions

--- third.js
+++ third.js
@@ -34,23 +34,64 @@ class Player {
     return all_equal;
     }
     poker() {
-        let poker = 0,
-            ordered = this.hand.map(x => Object.values(x)[1]).sort(),
-            first_four = this.hand.splice(0, 4),
-            last_four = this.hand.splice(1, 4);
-            poker += (first_four.every(x => first_four[0] == x)) ? first_four.reduce((acc, curr) => acc + curr) : (last_four.every(y => last_four[0] == y)) ? last_four.reduce((acc, curr) => acc + curr) : 0;
-        return poker;
-      }
+      let poker = 0;
+      let ordered = this.hand.map(x => Object.values(x)[1]).sort();
+      let first_four = ordered.splice(0, 4);
+      ordered = this.hand.map(x => Object.values(x)[1]).sort();
+      let last_four = ordered.splice(1, 4);
+      poker = (first_four.every(x => first_four[0] == x)) 
+        ? first_four.reduce((acc, curr) => acc + curr) 
+        : (last_four.every(y => last_four[0] == y)) 
+        ? last_four.reduce((acc, curr) => acc + curr) 
+        : 0;
+      return poker;
+    }
     house() {
       let ordered = this.hand.map(x => Object.values(x)[1]).sort();
       
-      let triplet = (ordered.slice(0, 3).every(x => ordered[0] == x)) ? ordered.slice(0, 3).reduce((acc, curr) => acc + curr) : (ordered.slice(2, 3).every(y => ordered[-1] == y)) ? ordered.slice(2, 3).reduce((acc, curr) => acc + curr) : 0;
-      let tuple = (ordered.slice(0, 2).every(a => ordered[0] == a)) ? ordered.slice(0, 2).reduce((acc, curr) => acc + curr) : (ordered.slice(3, 2).every(b => ordered[-1] == b)) ? ordered.slice(3, 2).reduce((acc, curr) => acc + curr) : 0;
-      
-      let result_house = ((triplet !=0) && (tuple !=0)) ? triplet + tuple : 0;
+      let triplet = (ordered.slice(0, 3).every(x => ordered[0] == x)) ? ordered.slice(0, 3).reduce((acc, curr) => acc + values.indexOf(curr)) : (ordered.slice(2, 3).every(y => ordered[-1] == y)) ? ordered.slice(2, 3).reduce((acc, curr) => acc + values.indexOf(curr)) : 0;
+      let double = (ordered.slice(0, 2).every(a => ordered[0] == a)) ? ordered.slice(0, 2).reduce((acc, curr) => acc + values.indexOf(curr)) : (ordered.slice(3).every(b => ordered[-1] == b)) ? ordered.slice(3).reduce((acc, curr) => acc + values.indexOf(curr)) : 0;      
+      let result_house = ((triplet !=0) && (double !=0)) ? triplet + double : 0;
       
       return result_house;
       
+    }
+    flush() {
+      let suits = this.hand.map(x => Object.values(x)[0]);
+      let result_flush = (suits.every(x => x == suits[0])) ? suits.reduce(((acc, curr) => acc + suit.indexOf(curr)), suit.indexOf(suits[0])) : 0; 
+      return result_flush;
+    }
+    straight() {
+      let ordered = this.hand.map(x => Object.values(x)[1]).sort();
+      let straight_result = (ordered.every((x, i) => x == Object.values(this.hand[i])[1])) ? ordered.reduce((acc, curr) => acc + curr) : 0;
+      return straight_result;
+    }
+    three() {
+      let ordered = this.hand.map(x => Object.values(x)[1]).sort();
+      let three_result = (ordered.some((x, i) => x == ordered[i+1] && x == ordered[i+2])) ? ordered.filter((z, i) => z == ordered[i+1] && z == ordered[i+2]) : 0;
+      return three_result;
+    }
+    two_pairs() {
+      let pairs = [],
+          ordered = this.hand.map(x => Object.values(x)[1]).sort();
+      for (let i = 0; i < ordered.length; i++) {
+        if ((ordered[i] == ordered[i+1]) && (ordered[i] != ordered[i+1])) {pairs.push(values.indexOf(ordered[i]))}
+      }
+      return pairs;
+      
+    }
+    pair() {
+      let ordered = this.hand.map(x => Object.values(x)[1]).sort(),
+          result_pair = 0;
+      for (let i = 0; i < ordered.length; i++) {
+        if ((ordered[i] == ordered[i+1]) && (ordered[i] != ordered[i+1])) {result_pair = values.indexOf(ordered[i])}
+      }
+      return result_pair;
+      
+    }
+    highest() {
+      let ordered = this.hand.map(x => Object.values(x)[1]).sort();
+      return values.indexOf(ordered[-1])
     }
 }
 
@@ -86,6 +127,8 @@ function straight_flush(p1, p2) {
   }
 
   function poker(p1, p2) {
+  
+  
     let p1_poker = p1.poker();
     let p2_poker = p2.poker();
     let game_over = false;
@@ -103,6 +146,7 @@ function straight_flush(p1, p2) {
   }
 
   function full_house(p1, p2) {
+  
     let p1_house = p1.house();
     let p2_house = p2.house();
     let game_over = false;
@@ -117,9 +161,118 @@ function straight_flush(p1, p2) {
         console.log(`${winner} wins with a Full House!`)
         game_over = true;
       }
-    return game_over;
     
+    return game_over;
   }
+
+  function flush (p1, p2) {
+    let p1_flush = p1.flush();
+    let p2_flush = p2.flush();
+    let game_over = false;
+    
+    if ((p1_flush != 0) && (p2_flush != 0)) {
+      let winner = (p1_flush > p2_flush) ? p1.name : p2.name;
+      console.log(`${winner} wins with a Flush!`)
+      game_over = true;
+    }
+    else if ((p1_flush != 0) || (p2_flush != 0)){
+        let winner = (p1_flush != 0) ? p1.name : p2.name;
+        console.log(`${winner} wins with a Flush!`)
+        game_over = true;
+      }
+    
+    return game_over;
+  }
+
+  function straight(p1, p2) {
+    let p1_straight = p1.straight();
+    let p2_straight = p2.straight();
+    let game_over = false;
+    
+    if ((p1_straight != 0) && (p2_straight != 0)) {
+      let winner = (p1_straight > p2_straight) ? p1.name : p2.name;
+      console.log(`${winner} wins with a Straight Hand!`)
+      game_over = true;
+    }
+    else if ((p1_straight != 0) || (p2_straight != 0)){
+        let winner = (p1_straight != 0) ? p1.name : p2.name;
+        console.log(`${winner} wins with a Straight Hand!`)
+        game_over = true;
+      }
+    
+    return game_over;
+  }
+
+  function three(p1, p2) {
+    let p1_three = p1.three();
+    let p2_three = p2.three();
+    let game_over = false;
+    
+    if ((p1_three != 0) && (p2_three != 0)) {
+      let winner = (p1_three > p2_three) ? p1.name : p2.name;
+      console.log(`${winner} wins with a Three of a Kind!`)
+      game_over = true;
+    }
+    else if ((p1_three != 0) || (p2_three != 0)){
+        let winner = (p1_three != 0) ? p1.name : p2.name;
+        console.log(`${winner} wins with a Three of a Kind!`)
+        game_over = true;
+      }
+    
+    return game_over;
+  }
+
+  function two_pairs(p1, p2) {
+    let p1_two_pairs = p1.two_pairs();
+    let p2_two_pairs = p2.two_pairs();
+    let game_over = false;
+    if ((p1_two_pairs == []) && (p1_two_pairs == [])) {return game_over}
+    if ((p1_two_pairs[0] != p2_two_pairs[0]) && (p1_two_pairs[1] != p2_two_pairs[1])) {
+      let winner = (p1_two_pairs.reduce((acc, curr) => acc+curr) > p2_two_pairs.reduce((acc, curr) => acc+curr)) ? p1.name : p2.name;
+      console.log(`${winner} wins with a Two Pairs!`)
+      game_over = true;
+    }
+    else if ((p1_two_pairs[0] == p2_two_pairs[0]) && (p1_two_pairs[1] == p2_two_pairs[1])){
+      let remaining_p1 = p1.hand.map(x => Object.values(x)[1]).sort().filter(z => (z != p1_two_pairs[0]) && (z != p1_two_pairs[1]));
+      let remaining_p2 = p2.hand.map(x => Object.values(x)[1]).sort().filter(z => (z != p1_two_pairs[0]) && (z != p1_two_pairs[1]));
+      let winner = (remaining_p1 > remaining_p2) ? p1.name : p2.name;
+        console.log(`${winner} wins with a Two pairs!`)
+        game_over = true;
+      }
+    else if (p1_two_pairs[0] == p2_two_pairs[0]) {
+      let winner = (p1_two_pairs[0] > p2_two_pairs[0]) ? p1.name : p2.name;
+      console.log(`${winner} wins with a Two Pairs!`)
+      game_over = true;}
+    
+    
+    return game_over;
+  }
+      
+  function pair(p1, p2) {
+    let p1_pair = p1.pair();
+    let p2_pair = p2.pair();
+    let game_over = false;
+    
+    if ((p1_pair != 0) && (p2_pair != 0)) {
+      let winner = (p1_pair > p2_pair) ? p1.name : p2.name;
+      console.log(`${winner} wins with a Pair!`)
+      game_over = true;
+    }
+    
+    return game_over;
+  }
+
+  function highest(p1, p2) {
+    let p1_highest = p1.highest();
+    let p2_highest = p2.highest();
+    let game_over = false;
+    
+    let winner = (p1_highest > p2_highest) ? p1.name : p2.name;
+    console.log(`${winner} wins with a Highest!`)
+    game_over = true;
+    
+    return game_over;
+  };
 
 function showdown () {
 
@@ -139,32 +292,14 @@ function showdown () {
     let player_one = new Player(prompt("Please enter first player", "San Gennaro"), hand_one),
         player_two = new Player(prompt("Please enter second player", "Cheesus"), hand_two);
 
-    straight_flush(player_one, player_two);
-    poker(player_one, player_two);
-
-
+        straight_flush(player_one, player_two);
+        poker(player_one, player_two);
+        full_house(player_one, player_two);
+        flush(player_one, player_two);
+        straight(player_one, player_two);
+        three(player_one, player_two);
+        two_pairs(player_one, player_two);
+        pair(player_one, player_two);
+        highest(player_one, player_two);
   }
 
-  
-
-
-
-* High Card (Carta Más Alta): Para manos que no entran en ninguna de las manos superior, 
-el ganador es aquel que tiene la carta más alta. Si se produce un empate entonces se compara la siguiente carta más alta y así sucesivamente. 
-
-* Pair (Parejas): 2 de las 5 cartas de la mano tienen el mismo valor. 
-Si las dos manos tienen pareja, entonces gana la que tenga la pareja más alta. 
-Si ambas parejas son iguales entonces gana el que tenga la carta más alta. 
-
-* Two Pairs (Dobles Parejas): La mano contiene 2 parejas diferentes. 
-Si las dos manos tienen dobles parejas diferentes entonces gana aquella que tenga la pareja más alta. 
-Si las dos manos tienen las mismas parejas entonces se compara la otra pareja. Si ambas manos tiene las mismas parejas entonces gana el que tenga la carta más alta restante. 
-
-* Three of a Kind (Trio): 3 cartas de la mano tienen el mismo valor. 
-Gana la mano que tiene las 3 cartas con mayor valor. 
-
-* Straight (Escalera): La mano contiene 5 cartas consecutivas. 
-Si las dos manos tienen escalera entonces gana la que tiene la carta más alta. 
-
-* Flush (Color): La mano tiene 5 cartas con la misma cara. 
-Si ambas manos tienen escalera entonces gana el que tenga la carta más alta. 
